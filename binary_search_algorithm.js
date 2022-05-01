7a9648320f37f13856df836b8994e48655b5b91d
vincent-zola <vincent.del@protonmail.com>
2022-05-01 14:52:54 +0200

    formated

--- binary_search_algorithm.js
+++ binary_search_algorithm.js
@@ -1,21 +1,18 @@
 // * ========== Binary Search Algorithm ==========
 
-const values = [0,1,2,3,4,5,6,7,8,9,10]
+const values = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 //* naive approach
 
-
-
-
 const search = (val, arr) => {
-  for(let i = 0; i < arr.length; i++) {
+  for (let i = 0; i < arr.length; i++) {
     console.log(i);
-    if (val === arr[i]){
+    if (val === arr[i]) {
       return i;
     }
   }
   return -1;
-}
+};
 
 // console.log(search(7, values));
 
@@ -25,21 +22,19 @@ const binary = (val, arr) => {
   let lower = 0;
   let upper = arr.length - 1;
 
-  while(lower <= upper){
+  while (lower <= upper) {
     console.log("try");
-      const middle = lower + Math.floor((upper - lower)/2);
-      if(val === arr[middle]){
-        return middle;
-      }
-      if(val<arr[middle]){
-        upper = middle -1;
-      }else{
-        lower = middle + 1;
-      }
+    const middle = lower + Math.floor((upper - lower) / 2);
+    if (val === arr[middle]) {
+      return middle;
+    }
+    if (val < arr[middle]) {
+      upper = middle - 1;
+    } else {
+      lower = middle + 1;
+    }
   }
   return -1;
-}
+};
 
 console.log(binary(7, values));
-
-
